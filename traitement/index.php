8f797928c47166ecdf34f20a210efa3c2052143d
Zoumana-Sow <62885153+Zoumana-Sow@users.noreply.github.com>
2020-06-02 01:04:03 +0000

    Add files via upload

---
+++ traitement/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quizz</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <link rel="stylesheet" href="../css/styles.css">
+    
+</head>
+<body>
+<header class="container-fluid" style="background-color: #F2F2F2;">
+   <div class="row haut">
+                <div class="col-lg-12">
+                 <img src="../images/logo-QuizzSA.png">
+              </div>
+            
+   </div>  
+</header>     
+<section class="contenair contenu" style="background-color: #FFC000;">
+ <?php
+ require_once("../pages/connexion.php")
+
+
+ ?>       
+ </section>
+   
+<footer class="contenair-fluid footer" style="background-color: #F2F2F2;"> 
+             <div class="row">
+               <p>Copyright © Zoumana Sow</p>
+             </div>
+</footer>      
+</body> 
+</html>
